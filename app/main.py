dace1d3e4d67e6ea9bef76f1d512d05cb601fb63
RickyHao <a471558277@gmail.com>
2018-02-16 23:29:54 +0800

    Use POST to delete file.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ from .entry import Scaner
 
 app = Flask(__name__)
 
+#@app.before_request
+#def before_request():
+#    print("bf:"+request.form['path'])
+
 @app.route('/files/')
 def index():
     return render_template('index.html', path='/', entries=Scaner(root_path).entries)
@@ -24,8 +28,11 @@ def show(path):
         return send_from_directory(os.path.dirname(real_path), os.path.basename(real_path), as_attachment=True)
 
 
-@app.route('/files/delete/<path:path>')
-def delete(path):
+@app.route('/files/delete', methods=['POST'])
+def delete():
+    path = request.get_json().get('path')
+    if path is None:
+        return redirect(url_for('index'))
     path = os.path.join(root_path, path)
     parent_path = os.path.relpath(os.path.dirname(path), root_path)
     if not os.path.exists(path):
