16c8b83b26ca0967cde6ce29fa778149469f769d
RickyHao <a471558277@gmail.com>
2018-02-17 11:41:09 +0800

    Add entry.type to index.html, and add _get_size to entry class.

--- app/entry.py
+++ app/entry.py
@@ -12,9 +12,26 @@ class Entry:
         self.is_dir = entry.is_dir()
         self.is_file = entry.is_file()
         self.mtime = datetime.fromtimestamp(entry.stat().st_mtime).ctime()
-        self.type = 'Directory' if self.is_dir else 'File'
+        self.type = 'D' if self.is_dir else 'F'
         self.relpath = os.path.relpath(self.path, root_path)
-        self.size = str(entry.stat().st_size)
+        self.size = self._get_size(entry)
+
+    def _get_size(self, entry):
+        size = entry.stat().st_size
+        unit = 'B'
+        if size < 1024:
+            unit = 'B'
+        elif size < 1048576:
+            size = size/1024
+            unit = 'K'
+        elif size < 1073741824:
+            size = size/1048576
+            unit = 'M'
+        elif size < 1099511627776:
+            size = size/1073741824
+            unit = 'G'
+
+        return '{0:.2f} {1}'.format(size, unit)
 
     def _to_unicode(self, string):
         return string.decode()
